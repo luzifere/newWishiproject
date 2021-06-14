8e11cf0e4e1efd1fde8c5f5610413601ff606ba7
luzifere <luzifere@gmail.com>
2021-06-14 20:21:42 +0300

    add style of onboarding

--- src/main/java/PageObjects/OnBoardingPage.java
+++ src/main/java/PageObjects/OnBoardingPage.java
@@ -23,6 +23,83 @@ public class OnBoardingPage extends BasePage {
     List<WebElement> bodytype;
     @FindBy(xpath = "//*[label]")
     WebElement bodyType;
+    @FindBy(xpath = "//*[@id='skip']")
+    WebElement skipbutton;
+    @FindBy(xpath = "(//button[contains(text(), 'NEXT')])[1]")
+    WebElement nextbutton;
+    @FindBy(xpath = "//label[@for='chic0']")
+    WebElement hellnochic;
+    @FindBy(xpath = "//label[@for='chic1']")
+    WebElement somtimechic;
+    @FindBy(xpath = "//label[@for='chic2']")
+    WebElement loveitchic;
+    @FindBy(xpath = "//label[@for='Boho0']")
+    WebElement hellnoboho;
+    @FindBy(xpath = "//label[@for='Boho1']")
+    WebElement somtimeboho;
+    @FindBy(xpath = "//label[@for='Boho2']")
+    WebElement loveitboho;
+    @FindBy(xpath = "//label[@for='Classic0']")
+    WebElement hellnoClassic;
+    @FindBy(xpath = "//label[@for='Classic1']")
+    WebElement somtimeClassic;
+    @FindBy(xpath = "//label[@for='Classic2']")
+    WebElement loveitClassic;
+    @FindBy(xpath = "//label[@for='Rock0']")
+    WebElement hellnorock;
+    @FindBy(xpath = "//label[@for='Rock1']")
+    WebElement somtimerock;
+    @FindBy(xpath = "//label[@for='Rock2']")
+    WebElement loveitrock;
+    @FindBy(xpath = "//label[@for='Trendy0']")
+    WebElement hellnotrendy;
+    @FindBy(xpath = "//label[@for='Trendy1']")
+    WebElement somtimetrendy;
+    @FindBy(xpath = "//label[@for='Trendy2']")
+    WebElement loveittrendy;
+    @FindBy(xpath = "//label[@for='streetwear0']")
+    WebElement hellnostreetwear;
+    @FindBy(xpath = "//label[@for='streetwear1']")
+    WebElement somtimestreetwear;
+    @FindBy(xpath = "//label[@for='streetwear2']")
+    WebElement loveitstreetwear;
+    @FindBy(xpath = "//label[@for='sexy0']")
+    WebElement hellnosexy;
+    @FindBy(xpath = "//label[@for='sexy1']")
+    WebElement somtimesexy;
+    @FindBy(xpath = "//label[@for='sexy2']")
+    WebElement loveitsexy;
+    @FindBy(xpath = "//label[@for='eclectic0']")
+    WebElement hellnoeclectic;
+    @FindBy(xpath = "//label[@for='eclectic1']")
+    WebElement somtimeeclectic;
+    @FindBy(xpath = "//label[@for='eclectic2']")
+    WebElement loveiteclectic;
+    @FindBy(xpath = "//label[@for='Tomboy0']")
+    WebElement hellnotomboy;
+    @FindBy(xpath = "//label[@for='Tomboy0']")
+    WebElement somtimetomboy;
+    @FindBy(xpath = "//label[@for='Tomboy0']")
+    WebElement loveittomboy;
+    @FindBy(xpath = "//label[@for='minimal0']")
+    WebElement hellnominimal;
+    @FindBy(xpath = "//label[@for='minimal0']")
+    WebElement somtimeminimal;
+    @FindBy(xpath = "//label[@for='minimal0']")
+    WebElement loveitminimal;
+    @FindBy(xpath = "//label[@for='Feminine0']")
+    WebElement hellnofeminime;
+    @FindBy(xpath = "//label[@for='Feminine0']")
+    WebElement somtimefeminime;
+    @FindBy(xpath = "//label[@for='Feminine0']")
+    WebElement loveitfeminime;
+    @FindBy(xpath = "(//div[@class='row']/..//div[@class='answers row']//label)[1]")
+    WebElement rhellno;
+    @FindBy(xpath = "(//div[@class='row']/..//div[@class='answers row']//label)[2]")
+    WebElement rsomtime;
+    @FindBy(xpath = "(//div[@class='row']/..//div[@class='answers row']//label)[3]")
+    WebElement rloveit;
+
 
 
     public OnBoardingPage(WebDriver driver) {
@@ -53,8 +130,15 @@ public class OnBoardingPage extends BasePage {
         WaitElementDosplayed(backbutton);
         click(backbutton);
     }
-
-    public void BodyTypePageLOaded() {
+    public void ClickSkip() {
+        WaitElementDosplayed(skipbutton);
+        click(skipbutton);
+    }
+    public void ClickNext() {
+        WaitElementDosplayed(nextbutton);
+        click(nextbutton);
+    }
+    public void BodyTypePageLoaded() {
         WaitElementDosplayed(bodyType);
     }
 
@@ -72,9 +156,141 @@ public class OnBoardingPage extends BasePage {
                 System.out.println(element);
                 WaitElementDosplayed(element);
                 click(element);
-                sleep(1000);
             }
         }
     }
+    public void SelectHellNoChick() {
+        WaitElementDosplayed(hellnochic);
+        click(hellnochic);
+    }
+    public void SelectSomtimeChick() {
+        WaitElementDosplayed(somtimechic);
+        click(somtimechic);
+    }
+    public void SelectLoveItChick() {
+        WaitElementDosplayed(loveitchic);
+        click(loveitchic);
+    }
+    public void SelectHellNoBoho() {
+        WaitElementDosplayed(hellnoboho);
+        click(hellnoboho);
+    }
+    public void SelectSomtimeBoho() {
+        WaitElementDosplayed(somtimeboho);
+        click(somtimeboho);
+    }
+    public void SelectLoveItBoho() {
+        WaitElementDosplayed(loveitboho);
+        click(loveitboho);
+    }
+    public void SelectHellNoClassic() {
+        WaitElementDosplayed(hellnoClassic);
+        click(hellnoClassic);
+    }
+    public void SelectSomtimeClassic() {
+        WaitElementDosplayed(somtimeClassic);
+        click(somtimeClassic);
+    }
+    public void SelectLoveItClassic() {
+        WaitElementDosplayed(loveitClassic);
+        click(loveitClassic);
+    }
+    public void SelectHellNoRock() {
+        WaitElementDosplayed(hellnorock);
+        click(hellnorock);
+    }
+    public void SelectSomtimeRock() {
+        WaitElementDosplayed(somtimerock);
+        click(somtimerock);
+    }
+    public void SelectLoveItRock() {
+        WaitElementDosplayed(loveitrock);
+        click(loveitrock);
+    }
+    public void SelectHellNoTrendy() {
+        WaitElementDosplayed(hellnotrendy);
+        click(hellnotrendy);
+    }
+    public void SelectSomtimeTrendy() {
+        WaitElementDosplayed(somtimetrendy);
+        click(somtimetrendy);
+    }
+    public void SelectLoveItTrendy() {
+        WaitElementDosplayed(loveittrendy);
+        click(loveittrendy);
+    }
+    public void SelectHellNoStreetWear() {
+        WaitElementDosplayed(hellnostreetwear);
+        click(hellnostreetwear);
+    }
+    public void SelectSomtimeStreetWear() {
+        WaitElementDosplayed(somtimestreetwear);
+        click(somtimestreetwear);
+    }
+    public void SelectLoveItStreetWear() {
+        WaitElementDosplayed(loveitstreetwear);
+        click(loveitstreetwear);
+    }
+    public void SelectHellNoSexy() {
+        WaitElementDosplayed(hellnosexy);
+        click(hellnosexy);
+    }
+    public void SelectSomtimeSexy() {
+        WaitElementDosplayed(somtimesexy);
+        click(somtimesexy);
+    }
+    public void SelectLoveItSexy() {
+        WaitElementDosplayed(loveitsexy);
+        click(loveitsexy);
+    }
+    public void SelectHellNoElectic() {
+        WaitElementDosplayed(hellnoeclectic);
+        click(hellnoeclectic);
+    }
+    public void SelectSomtimeElectic() {
+        WaitElementDosplayed(somtimeeclectic);
+        click(somtimeeclectic);
+    }
+    public void SelectLoveItElectic() {
+        WaitElementDosplayed(loveiteclectic);
+        click(loveiteclectic);
+    }
+    public void SelectHellNoTomboy() {
+        WaitElementDosplayed(hellnotomboy);
+        click(hellnotomboy);
+    }
+    public void SelectSomtimeTomboy() {
+        WaitElementDosplayed(somtimetomboy);
+        click(somtimetomboy);
+    }
+    public void SelectLoveItTomboy() {
+        WaitElementDosplayed(loveittomboy);
+        click(loveittomboy);
+    }
+    public void SelectHellNoMinimal() {
+        WaitElementDosplayed(hellnominimal);
+        click(hellnominimal);
+    }
+    public void SelectSomtimeMinimal() {
+        WaitElementDosplayed(somtimeminimal);
+        click(somtimeminimal);
+    }
+    public void SelectLoveItMinimal() {
+        WaitElementDosplayed(loveitminimal);
+        click(loveitminimal);
+    }
+    public void SelectHellNoFeminime() {
+        WaitElementDosplayed(hellnofeminime);
+        click(hellnofeminime);
+    }
+    public void SelectSomtimeFeminime() {
+        WaitElementDosplayed(somtimefeminime);
+        click(somtimefeminime);
+    }
+    public void SelectLoveItFeminime() {
+        WaitElementDosplayed(loveitfeminime);
+        click(loveitfeminime);
+    }
+
 }
 
--- src/main/java/TestsObjects/OnBoardingTest.java
+++ src/main/java/TestsObjects/OnBoardingTest.java
@@ -14,25 +14,23 @@ import java.util.Random;
 public class OnBoardingTest extends setup {
 
 
-
     @Attachment
     @Story("Do correct signup")
     @Severity(SeverityLevel.CRITICAL)
     @Test(priority = 1, groups = {"sanity-group"})
-    public void DoSignUp()
-    {
+    public void DoSignUp() {
 
         Random num = new Random();
         int number = 1000000;
-        for (int counter = 5800000; counter<=10000000;counter++)
+        for (int counter = 5800000; counter <= 10000000; counter++)
             number = num.nextInt(700000);
         SignUpPage sign = new SignUpPage(driver);
         sign.ClickSignUpButton();
         sign.ClearFullName();
-        sign.FillFullName( "test test");
+        sign.FillFullName("test test");
         sign.ClearEmail();
         String newusermaile = this.configFileReader.getnewusermaile();
-        sign.FillEmail(newusermaile+ number);
+        sign.FillEmail(newusermaile + number);
         //sign.FillEmail("wishitestyinon@wishitest.com" + number);
         sign.Clearpassword();
         String password = this.configFileReader.getpassword();
@@ -41,22 +39,56 @@ public class OnBoardingTest extends setup {
         String expected = "LET'S GET STYLING";
         String actual = sign.LetsGetButtonDisplayed();
         System.out.println(actual);
-        Assert.assertEquals(actual,expected);
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected);
 
     }
+
     @Attachment
     @Story("Select Famale")
     @Severity(SeverityLevel.CRITICAL)
     @Test(priority = 2, groups = {"sanity-group"})
-    public void SelectFamale()
-    {
+    public void SelectFamale() {
         OnBoardingPage ob = new OnBoardingPage(driver);
         ob.LetsGet();
         ob.SelectMale();
         ob.ClickBack();
         ob.SelectFamale();
-        ob.BodyTypePageLOaded();
+        ob.BodyTypePageLoaded();
+
+    }
+
+    @Attachment
+    @Story("Select Bodytype")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(priority = 3, groups = {"sanity-group"})
+    public void SelectBodytype() {
+        OnBoardingPage ob = new OnBoardingPage(driver);
+        ob.ClickSkip();
+        ob.ClickBack();
         ob.SelectBodyType("petite");
+        ob.SelectBodyType("fit");
+        ob.SelectBodyType("tall");
+        ob.SelectBodyType("petite");
+        ob.ClickNext();
+    }
+    @Attachment
+    @Story("Select Bodytype")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(priority = 4, groups = {"sanity-group"})
+    public void SelctStyle() {
+        OnBoardingPage ob = new OnBoardingPage(driver);
+        ob.SelectHellNoChick();
+        ob.SelectLoveItBoho();
+        ob.SelectSomtimeClassic();
+        ob.SelectHellNoRock();
+        ob.SelectLoveItTrendy();
+        ob.SelectSomtimeStreetWear();
+        ob.SelectHellNoSexy();
+        ob.SelectSomtimeElectic();
+        ob.SelectLoveItTomboy();
+        ob.SelectSomtimeMinimal();
+        ob.SelectHellNoFeminime();
+
     }
 }
